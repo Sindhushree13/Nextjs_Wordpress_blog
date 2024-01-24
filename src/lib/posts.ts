f92d2c3e616f413a5caf45edfa3c7d8b91d984ff
Sindhushree13 <sindhushree58@gmail.com>
2024-01-24 15:04:37 +0530

    changes

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -10,7 +10,7 @@ export async function getPostList(endCursor = null, taxonomy = null) {
 
     const query = {
         query: `query getAllPosts {
-            posts() {
+            posts {
               nodes {
                 date
                 slug
